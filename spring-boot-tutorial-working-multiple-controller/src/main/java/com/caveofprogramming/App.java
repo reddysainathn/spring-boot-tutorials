700d4508786852ff972c5ec89430d623df324825
Sainath Narahari <sainath.narahari@ge.com>
2017-04-21 21:01:49 -0500

    Multiple Controllers , with Packaging of controllers.

--- spring-boot-tutorial-working-multiple-controller/src/main/java/com/caveofprogramming/App.java
+++ spring-boot-tutorial-working-multiple-controller/src/main/java/com/caveofprogramming/App.java
@@ -2,15 +2,18 @@ package com.caveofprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App extends SpringBootServletInitializer{
 
+/*@EnableAutoConfiguration
+@ComponentScan --> Two annotation are together as @SpringBootApplication*/
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
@@ -18,16 +21,6 @@ public class App extends SpringBootServletInitializer{
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(App.class);
-	}
-	
-	@RequestMapping("/about")
-	String about() {
-		return "about";
-	}
-	
-	@RequestMapping("/")
-	String home() {
-		return "home";
 	}
 
 }
